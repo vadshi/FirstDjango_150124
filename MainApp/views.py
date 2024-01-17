6cc90d3582e1735cde42677503d2dfea0ecc4ad4
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-01-17 21:27:51 +0300

    Add repr to Item

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
-
+from django.core.exceptions import ObjectDoesNotExist
 from MainApp.models import Item
 
 
