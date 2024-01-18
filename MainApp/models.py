6ddba6cd73fead943b88d7c299a4bcb5da70ac58
Vadim <vadimx30@gmail.com>
2024-01-18 19:23:48 +0300

    Done. M3:part2 task1-3

--- MainApp/models.py
+++ MainApp/models.py
@@ -7,6 +7,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
+    description = models.TextField(max_length=200, default="Базовое описание")
 
     def __repr__(self):
         return f'Item({self.name}, {self.brand}, {self.count})'
