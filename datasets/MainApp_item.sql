d7ada53080cc21b30b74833ba2d03ab759499e30
Vadim <vadimx30@gmail.com>
2024-01-18 21:27:52 +0300

    Done. M4:part1 full

---
+++ datasets/MainApp_item.sql
@@ -0,0 +1,6 @@
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (1,'Кроссовки','abibas',10,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (2,'Куртка кожаная','Nike',2,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (3,'Coca-cola 1 литр','Coca-Cola',12,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (4,'Картофель фри','Точка',0,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (5,'Кепка','ООО Текстиль',124,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (6,'Computer','IBM',10,'IBM-PC 286');
